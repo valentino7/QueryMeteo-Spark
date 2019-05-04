055e56fba407d5fb4206a24b9eab859e2db684f5
valentino7 <perrone.valentino@gmail.com>
2019-05-04 19:10:35 +0200

    pre-processing

--- src/main/java/first_query/Query1.java
+++ src/main/java/first_query/Query1.java
@@ -6,12 +6,15 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
 import scala.Tuple2;
 
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class Query1 {
@@ -42,8 +45,13 @@ public class Query1 {
 
         JavaRDD<Record> weatherRDD = Query1Preprocessing.preprocessDataset(sc);
 
-        JavaPairRDD<LocalDate, Record> pairs =
-                weatherRDD.mapToPair(s -> new Tuple2<>(s.getDate(), s));
+        /*JavaPairRDD<LocalDate, HashMap<String,String>> pairs =
+                weatherRDD.mapToPair(s -> new Tuple2<>(s.getDate(), s.getWeather_city()));*/
+
+
+
+        JavaPairRDD<LocalDate, ArrayList<String>> pairs = weatherRDD.flatMapToPair(new CityExtractor());
+
 
 
 
@@ -51,12 +59,38 @@ public class Query1 {
         //JavaRDD<Record> d = weatherRDD.filter(m -> m.getDate().getMonthValue()> 2 && m.getDate().getMonthValue()<6);
 
         for(Tuple2 r: pairs.collect()) {
-            System.out.println(r._2 + "\n");
+            System.out.println(r + "\n");
 
         }
 
         //data.saveAsObjectFile(outputPath);
 
         sc.stop();
+    }
+
+    private static class CityExtractor implements PairFlatMapFunction<Record, LocalDate, ArrayList<String>> {
+
+        @Override
+        public Iterator<Tuple2<LocalDate,ArrayList<String>>> call(Record record) throws Exception {
+
+            ArrayList<Tuple2<LocalDate,ArrayList<String>>> results = new ArrayList<>();
+            //Iterator it = record.getWeather_city().entrySet().iterator();
+
+
+            for (Map.Entry<String, String> entry : record.getWeather_city().entrySet()) {
+                ArrayList<String> e= new ArrayList<>();
+                e.add(entry.getKey());
+                e.add(entry.getValue());
+
+
+                Tuple2<LocalDate,ArrayList<String>> result = new Tuple2<>(record.getDate(),e );
+                results.add(result);
+
+
+            }
+
+            return results.iterator();
+
+        }
     }
 }
