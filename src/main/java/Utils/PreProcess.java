a6a75335554b58527a20201cb3cb5eff095b42cf
anthony <pusceddu.anthony@gmail.com>
2019-05-10 13:24:33 +0200

    query 2 update

--- src/main/java/Utils/PreProcess.java
+++ src/main/java/Utils/PreProcess.java
@@ -3,24 +3,47 @@ package Utils;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.geonames.*;
 import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class PreProcess {
 
-    public static void executeProcess(JavaSparkContext sc) {
+    public static Map<String, String> executeProcess(JavaSparkContext sc) {
 
         JavaRDD<String> city_attributes = sc.textFile(Constants.CITY_FILE);
-        JavaPairRDD<String, String> city_nations = city_attributes.mapToPair(s -> findNation(s));
-
-        city_nations.cache();
+        String firstLine = city_attributes.first();
+        JavaPairRDD<String, String> city_nations = city_attributes
+                .filter(v1 -> !v1.equals(firstLine))
+                .mapToPair(PreProcess::findNation);
+        return city_nations.collectAsMap();
     }
 
     private static Tuple2<String, String> findNation(String s) {
 
-        String state = null;
         // funzione per prendere la nazione tramite la stringa : chiamata ad un API oppure ricerca in un file in locale
 
-        return new Tuple2<>(s,state);
+        String[] arrays = s.split(",");
+        String city = arrays[0];
+        String lat = arrays[1];
+        String lon = arrays[2];
+
+        WebService.setUserName("anthony2801");
+        List<Toponym> result = new ArrayList<>();
+
+        try {
+
+            result = WebService.findNearbyPlaceName(Double.parseDouble(lat),Double.parseDouble(lon));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        return new Tuple2<>(city,result.get(0).getCountryName());
 
     }
 
--- src/main/java/spark/Query2.java
+++ src/main/java/spark/Query2.java
@@ -2,12 +2,14 @@ package spark;
 
 import Utils.Constants;
 import Utils.Context;
+import Utils.PreProcess;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 import org.apache.spark.api.java.function.PairFlatMapFunction;
+import org.apache.spark.api.java.function.PairFunction;
 import scala.Tuple2;
 import scala.Tuple3;
 
@@ -29,12 +31,10 @@ public class Query2 {
         List<String> citiesArray = new ArrayList<>(Arrays.asList(firstLine.split(",")));
         citiesArray.remove(0);
 
+        // hashmap <Città,Nazione>
+        Map<String,String> city_nations = PreProcess.executeProcess(sc);
 
 
-
-        //creazione file di citta-nazione in preprocessing attraverso le api
-        //caricare il file come hashmap,quindi abbiamo una variabile hashmap del tipo citta-nazione
-        //trovo la nazione data la citta
         JavaPairRDD<Tuple3<Integer,Integer,String>, Tuple2<Double, Double> > dataset = attributes_file
                 .filter( csvLine -> !csvLine.equals(firstLine) )
                 .flatMapToPair((PairFlatMapFunction<String, Tuple3<Integer, Integer, String>, Tuple2<Double,Double> >) s -> {
@@ -43,58 +43,84 @@ public class Query2 {
                     Date date = df.parse(strings[0]);
                     GregorianCalendar cal = new GregorianCalendar();
                     cal.setTime(date);
-                    Double d = 0.0;
+                    double d = 0.0;
                     List<Tuple2<Tuple3<Integer,Integer,String>,Tuple2<Double,Double> > > list = new ArrayList<>();
                     for (int i = 1; i < strings.length; i++) {
                         if(strings[i].isEmpty())
                             d=0.0;
                         else
-                            d=Double.valueOf(strings[i]);
+                            d= Double.parseDouble(strings[i]);
                         if (d>= 350)
                             d=d/1000;
                         if (d>=1000000)
                             d=d/10000;
-                        list.add( new Tuple2<>(new Tuple3<>(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), citiesArray.get(i-1) ), new Tuple2<>( d ,1.0) ) );
+                        list.add( new Tuple2<>(new Tuple3<>(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), city_nations.get(citiesArray.get(i-1)) ), new Tuple2<>( d ,1.0) ) );
                     }
                     return list.iterator();
                 })
                 .filter((Function<Tuple2<Tuple3<Integer, Integer, String>, Tuple2<Double, Double>>, Boolean>) v1 -> v1._2._1() != 0.0);
-        // Somme per stesso mese, città e anno
-        // (Anno,Mese,Città) -> (Somma di temperature, count temperature )
+        // Somme per stesso mese, Nazione e anno
+        // (Anno,Mese,Nazione) -> (Somma di temperature, count temperature )
         JavaPairRDD<Tuple3<Integer,Integer,String>, Tuple2<Double, Double> > sum_count = dataset.reduceByKey((tuple1, tuple2) -> new Tuple2<>(tuple1._1()+tuple2._1(), tuple1._2()+ tuple2._2()));
 
-        JavaPairRDD<Tuple3<Integer,Integer,String>, Double > average = sum_count.mapValues((Function<Tuple2<Double, Double>, Double>) v1 -> v1._1()/v1._2());
+        JavaPairRDD<Tuple3<Integer,Integer,String>, Double > average = sum_count
+                .mapValues((Function<Tuple2<Double, Double>, Double>) v1 -> v1._1()/v1._2());
 
 
-        JavaPairRDD<Tuple3<Integer,Integer,String>, Tuple2<Double, Double> > min_max = dataset.reduceByKey(new Function2<Tuple2<Double, Double>, Tuple2<Double, Double>, Tuple2<Double, Double>>() {
-            @Override
-            public Tuple2<Double, Double> call(Tuple2<Double, Double> v1, Tuple2<Double, Double> v2) throws Exception {
-                Double max;
-                Double min ;
-                if (v1._1() >= v2._1()) {
-                    max = v1._1();
-                }else{
-                    max = v2._1();
-                }
-                if (v1._1() <= v2._1()) {
-                    min = v1._1();
-                } else{
-                    min  = v2._1();
-                }
+        JavaPairRDD<Tuple3<Integer,Integer,String>, Tuple2<Double, Double> > min_max = dataset
+                .reduceByKey(new Function2<Tuple2<Double, Double>, Tuple2<Double, Double>, Tuple2<Double, Double>>() {
+                        @Override
+                        public Tuple2<Double, Double> call(Tuple2<Double, Double> v1, Tuple2<Double, Double> v2) throws Exception {
+                            Double max;
+                            Double min ;
+                            if (v1._1() > v2._1()) {
+                                max = v1._1();
+                            }else{
+                                max = v2._1();
+                            }
+                            if (v1._1() < v2._1()) {
+                                min = v1._1();
+                            } else{
+                                min  = v2._1();
+                            }
 
-                return new Tuple2<>(max,min);
-            }
-        });
+                            return new Tuple2<>(max,min);
+                        }
+                    });
 
-        Map<Tuple3<Integer,Integer,String>, Double >map = average.collectAsMap();
-        Map<Tuple3<Integer,Integer,String>, Tuple2<Double, Double> >map1 = min_max.collectAsMap();
-       /* for ( Tuple3<Integer,Integer,String> d : map.keySet()){
-            System.out.println(d + " -> " + map.get(d) );
-        }*/
+        JavaPairRDD<Tuple3<Integer,Integer,String>, Double > std_dev = dataset
+                .join(average)
+                .mapValues((Function<Tuple2<Tuple2<Double, Double>, Double>, Tuple2<Double, Double>>) v1 -> new Tuple2<>( v1._1()._1() - v1._2() , v1._1()._2()))
+                .reduceByKey((v1, v2) -> new Tuple2<>(Math.pow(v1._1(), 2),  v1._2()+v2._2()  ) )
+                .mapValues(new Function<Tuple2<Double, Double>, Double>() {
+                    @Override
+                    public Double call(Tuple2<Double, Double> v1) throws Exception {
+                        double d = Math.sqrt(v1._1()/v1._2());
+                        System.out.println(v1._1() + "\t"+ v1._2() + "\t" + d );
+                        return d;
+                    }
+                });
 
-        for ( Tuple3<Integer,Integer,String> d : map1.keySet()){
-            System.out.println(d + " -> " + map1.get(d) );
+
+     /*   Map < Tuple3 < Integer, Integer, String >, Double > avergeMap = average.collectAsMap();
+        Map<Tuple3<Integer,Integer,String>, Tuple2<Double, Double> >min_maxMap = min_max.collectAsMap();
+        Map<Tuple3<Integer,Integer,String>, Double > stdMap = std_dev.collectAsMap();
+
+        for ( Tuple3<Integer,Integer,String> d : avergeMap.keySet()){
+            System.out.println(d + " -> " + avergeMap.get(d) );
         }
+
+        System.out.println("----------------------------------------------------------------------");
+
+        for ( Tuple3<Integer,Integer,String> d : min_maxMap.keySet()){
+            System.out.println(d + " -> " + min_maxMap.get(d) );
+        }
+
+        System.out.println("----------------------------------------------------------------------");
+
+        for ( Tuple3<Integer,Integer,String> d : stdMap.keySet()){
+            System.out.println(d + " -> " + stdMap.get(d) );
+        }*/
 
         //average.saveAsTextFile("output");
 
