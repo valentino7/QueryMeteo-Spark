46594b092a57ba33089a1bd7e9784b930eaac322
valentino7 <perrone.valentino@gmail.com>
2019-05-16 02:44:38 +0200

    create automatic tabel mongo

---
+++ src/main/java/docker/hbase/hbase-docker-master/start-hbase-personal.sh
@@ -0,0 +1,2 @@
+#contattarlo all'indirizzo http://192.168.64.8:16010/master-status#userTables
+docker run --network=net valentino94/hbase
--- src/main/java/docker/mongo/mongo-client-start.sh
+++ src/main/java/docker/mongo/mongo-client-start.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-docker run -it --name mongo-client --network net mongo:xenial /bin/bash -c 'mongo mongo-server:27017'
+#mongo = immagine docker
+#docker pull mongo
+
+
+
+
+docker cp ./script-mongo.js mongo_server:/script-mongo.js
+docker exec -it mongo_server bash -c 'mongo < script-mongo.js'
+
+
+
+#docker run -it --name mongo-client --network net mongo /bin/bash -c 'mongo mongo_server:27017'
+
+
+
