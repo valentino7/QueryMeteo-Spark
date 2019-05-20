1f35f92868b2da318e968a2b22cd2f65cb24cd7b
valentino7 <perrone.valentino@gmail.com>
2019-05-20 15:27:28 +0200

    fix

--- src/main/java/docker/apache-nifi/nifi-run.sh
+++ src/main/java/docker/apache-nifi/nifi-run.sh
@@ -8,24 +8,25 @@ JAR=1.1.32/nifi-deploy-config-1.1.32.jar
 
 docker run -t -i -p $PORT:$PORT -e NIFI_WEB_HTTP_PORT="$PORT" -e HDFS_DEFAULTS_FS="$HDFS_DEFAULT_FS" --network=net -d --hostname=$HOSTNAME --name=nifi apache/nifi:latest
 
-docker cp ./config/core-site.xml nifi:/opt/nifi/core-site.xml
-docker cp ./config/hdfs-site.xml nifi:/opt/nifi/hdfs-site.xml
-docker cp ./hbase/core-site.xml nifi:/opt/nifi/core-site.xml
-docker cp ./hbase/hbase-site.xml nifi:/opt/nifi/hbase-site.xml
-docker cp ./data nifi:/opt/nifi/
-docker cp ./TemplateFinal.xml nifi:/TemplateFinal.xml
-docker cp ./nifi-deploy-config-1.1.32.jar nifi:/nifi-deploy-config-1.1.32.jar
+docker cp ./apache-nifi/config/core-site.xml nifi:/opt/nifi/core-site.xml
+docker cp ./apache-nifi/config/hdfs-site.xml nifi:/opt/nifi/hdfs-site.xml
+docker cp ./apache-nifi/hbase/core-site.xml nifi:/opt/nifi/core-site.xml
+docker cp ./apache-nifi/hbase/hbase-site.xml nifi:/opt/nifi/hbase-site.xml
+docker cp ./apache-nifi/data nifi:/opt/nifi/
+docker cp ./apache-nifi/TemplateV4.xml nifi:/TemplateV4.xml
+docker cp ./apache-nifi/nifi-deploy-config-1.1.32.jar nifi:/nifi-deploy-config-1.1.32.jar
 
 
 
 
 #printf 'waiting for NiFi...\n'
 NIFI_HOST=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' nifi)
+echo "$NIFI_HOST"
 until $(curl --output /dev/null --silent --head --fail http://$NIFI_HOST:$PORT); do
     printf '.'
     sleep 5
 done
 
-docker exec -it nifi java -jar /nifi-deploy-config-1.1.32.jar -nifi http://172.18.0.6:9999/nifi-api -conf /TemplateFinal.xml -m deployTemplate
+docker exec -it nifi java -jar /nifi-deploy-config-1.1.32.jar -nifi http://$NIFI_HOST:$PORT/nifi-api -conf /TemplateV4.xml -m deployTemplate
 
 
---
+++ submit-spark.sh
@@ -0,0 +1,1 @@
+$SPARK_HOME/bin/spark-submit --class MainQuery2 --master "local" target/handson-spark-1.0.jar 
