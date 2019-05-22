d06c2a59422908bbaa93286506e2b1c0d53e06ff
valentino7 <perrone.valentino@gmail.com>
2019-05-22 18:24:49 +0200

    script fix

---
+++ src/main/java/docker/wait_spark.py
@@ -0,0 +1,29 @@
+import sys, re,json,requests,time,re
+
+def send_request(url):
+    request = requests.get(url)
+    return request
+
+def wait_spark(ip):
+    url = 'http://'+str(ip)+':8080'
+    while(True):
+        page=send_request(url)
+        s = '<td>FINISHED</td>'
+        result = re.findall(s, page.text)
+        if len(result)==3:
+            break
+        sleep(10)
+        
+    print result
+    return True
+
+
+
+    
+
+if __name__ == '__main__':
+    wait_spark(sys.argv[1])
+
+
+
+ 
